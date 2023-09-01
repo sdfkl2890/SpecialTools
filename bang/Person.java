4b2d477c9790d508aeeeb5181ebc3e4c02775758
sdfkl2890 <70002827+sdfkl2890@users.noreply.github.com>
2023-09-01 11:05:52 +0800

    示例类

---
+++ bang/Person.java
@@ -0,0 +1,10 @@
+package bang;
+
+public class Person {
+    private String name;
+    public int age;
+
+    public void printName(String name){
+        System.out.println(name);
+    }
+}
---
+++ bang/Student.java
@@ -0,0 +1,8 @@
+package bang;
+
+public class Student extends Person{
+    public String name;
+    public String school;
+
+
+}
